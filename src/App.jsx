b7d8bfa803a6726d3269a71dc0aa90265ba69776
lightpotat0 <113602502+lightpotat0@users.noreply.github.com>
2026-06-23 17:16:25 -0300

    feat: correção de bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import { Link, Navigate, Route, Routes } from 'react-router-dom'
+import { Link, Navigate, Route, Routes } from "react-router-dom";
 
-import { NegociacaoProvider } from './context/NegociacaoContext'
+import { NegociacaoProvider } from "./context/NegociacaoContext";
 
-import Explorar from './pages/Explorar'
-import Home from './pages/Home'
+import CriarAnuncio from "./pages/CriarAnuncio";
+import DetalheAnuncio from "./pages/DetalheAnuncio";
+import EditarAnuncio from "./pages/EditarAnuncio";
+import Explorar from "./pages/Explorar";
+import Home from "./pages/Home";
+import "./styles/styles.css"
 
-import { ROUTES } from './routes'
+import { ROUTES } from "./routes";
 
 function App() {
   return (
@@ -17,42 +21,26 @@ function App() {
           </Link>
 
           <nav aria-label="Navegação principal">
-            <Link to={ROUTES.home}>
-              Início
-            </Link>
-
-            <Link to={ROUTES.explorar}>
-              Explorar
-            </Link>
+            <Link to={ROUTES.home}>Início</Link>
+            <Link to={ROUTES.explorar}>Explorar</Link>
+            <Link to={ROUTES.criarAnuncio}>Novo anúncio</Link>
           </nav>
         </header>
 
         <main>
           <Routes>
-            <Route
-              path={ROUTES.home}
-              element={<Home />}
-            />
+            <Route path={ROUTES.home} element={<Home />} />
+            <Route path={ROUTES.explorar} element={<Explorar />} />
+            <Route path={ROUTES.criarAnuncio} element={<CriarAnuncio />} />
+            <Route path={ROUTES.anuncioDetalhe} element={<DetalheAnuncio />} />
+            <Route path={ROUTES.editarAnuncio} element={<EditarAnuncio />} />
 
-            <Route
-              path={ROUTES.explorar}
-              element={<Explorar />}
-            />
-
-            <Route
-              path="*"
-              element={
-                <Navigate
-                  replace
-                  to={ROUTES.home}
-                />
-              }
-            />
+            <Route path="*" element={<Navigate replace to={ROUTES.home} />} />
           </Routes>
         </main>
       </div>
     </NegociacaoProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,15 @@
 export const ROUTES = Object.freeze({
-  home: '/',
-  explorar: '/explorar',
+  home: "/",
+  explorar: "/explorar",
+  criarAnuncio: "/anuncios/novo",
+  anuncioDetalhe: "/anuncios/:id",
+  editarAnuncio: "/anuncios/:id/editar",
 });
+
+export function anuncioPath(id) {
+  return `/anuncios/${id}`;
+}
+
+export function editarAnuncioPath(id) {
+  return `/anuncios/${id}/editar`;
+}
